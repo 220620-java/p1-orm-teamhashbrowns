0b6f7d44df0ae94c1a9793e4c4318c41235a9db8
fsanche3 <fsanche3@oswego.edu>
2022-07-15 21:35:42 -0400

    Addition of service package

---
+++ orm/src/main/java/hashbrowns/p1/driver/Main.java
@@ -0,0 +1,18 @@
+package hashbrowns.p1.driver;
+
+import hashbrowns.p1.data.ORMImpl;
+import hashbrowns.p1.models.Chef;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ORMImpl o = new ORMImpl();	
+		Chef c = new Chef (9,"2","pass","t");
+		
+
+
+
+	}
+
+}
